30300d9bf4ef43745ffbf9d9da7fcad75b13b664
DieterDraelants <dieter.draelants@student.ucll.be>
2020-03-24 14:14:49 +0100

    update

--- src/test/java/com/project/project/TaskServiceTest.java
+++ src/test/java/com/project/project/TaskServiceTest.java
@@ -1,4 +1,37 @@
 package com.project.project;
 
+import com.project.project.dto.TaskDTO;
+import com.project.project.model.Task;
+import com.project.project.service.TaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskServiceTest {
+
+    @Autowired
+    private TaskService taskService;
+
+    @Test
+    public void testGetTasks() {
+        //setup
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setTitle("task1");
+        taskDTO.setDescription("descri");
+        taskDTO.setDueDate(LocalDateTime.now());
+        taskService.addTask(taskDTO);
+
+        //method to be tested
+        List<Task> tasks = taskService.getAllTasks();
+
+        //Checks
+        assertNotNull(tasks);
+        assertFalse(tasks.isEmpty());
+        assertEquals(1, tasks.size());
+        assertNotNull(taskDTO);
+    }
 }
