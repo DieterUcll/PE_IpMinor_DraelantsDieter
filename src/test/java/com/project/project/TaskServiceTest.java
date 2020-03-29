5de9000ae71245b2dcd984efab7a2326ccd245fb
DieterDraelants <dieter.draelants@student.ucll.be>
2020-03-29 16:33:41 +0200

    noice

--- src/test/java/com/project/project/TaskServiceTest.java
+++ src/test/java/com/project/project/TaskServiceTest.java
@@ -1,12 +1,14 @@
 package com.project.project;
 
 import com.project.project.dto.TaskDTO;
+import com.project.project.model.SubTask;
 import com.project.project.model.Task;
 import com.project.project.service.TaskService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -50,5 +52,48 @@ public class TaskServiceTest {
         taskService.addTask(taskDTO);
 
         assertEquals(1,taskService.getTaskById(1).getId());
+    }
+
+    @Transactional
+    @Test
+    public void testEditTask() {
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setTitle("task1");
+        taskDTO.setDescription("descri");
+        taskDTO.setDueDate(LocalDateTime.of(2,2,2,1,1));
+        taskService.addTask(taskDTO);
+
+        Task t = new Task();
+        t.setId((long) 1);
+        t.setTitle("retarded");
+        t.setDescription("ok");
+        t.setDueDate(LocalDateTime.of(222,2,2,1,1));
+        taskService.editTask(t);
+
+        assertEquals("retarded",taskService.getTaskById(1).getTitle());
+        assertEquals("ok", taskService.getTaskById(1).getDescription());
+        assertEquals(LocalDateTime.of(222,2,2,1,1), taskService.getTaskById(1).getDueDate());
+    }
+
+
+
+    @Transactional
+    @Test
+    public void testAddSubtaskToTask() {
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setTitle("task1");
+        taskDTO.setDescription("descri");
+        taskDTO.setDueDate(LocalDateTime.of(2,2,2,1,1));
+        taskService.addTask(taskDTO);
+
+        SubTask subTask = new SubTask();
+        subTask.setTitle("subtask");
+        subTask.setDescription("rff");
+        subTask.setCorsTask((long) 1);
+
+
+        taskService.addSubTaskToTask(subTask);
+
+        assertEquals(1,taskService.getTaskById(1).getSubtasks().size());
     }
 }
